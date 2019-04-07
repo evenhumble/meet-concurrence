f2912a4fa38cf79706f05438c45399fcb7d7de8f
patrick <patrickwuke@163.com>
2019-04-07 15:35:21 +0800

    complete thread intro

--- concurrence-basic/src/main/java/io/hedwig/concurrence/basic/thread/ThreadStates.java
+++ concurrence-basic/src/main/java/io/hedwig/concurrence/basic/thread/ThreadStates.java
@@ -7,16 +7,55 @@ package io.hedwig.concurrence.basic.thread;
 public class ThreadStates {
 
   public static void main(String[] args) {
-    for (int i = 0; i < 10; i++) {
-      System.out.println(i);
-      try {
-        if (Thread.currentThread().isInterrupted()) {
-          return;
+    new Thread(new TimeWaitingThread(),"time-waiting").start();
+    new Thread(new WaitingThread(),"waiting").start();
+    new Thread(new BlockedThread(),"block-1").start();
+    new Thread(new BlockedThread(),"block-2").start();
+  }
+
+  static class TimeWaitingThread implements Runnable{
+
+    @Override
+    public void run() {
+      System.out.println("time waiting thread "+ Thread.currentThread().getId());
+      sleepInSeconds(20);
+    }
+  }
+
+  static class WaitingThread implements Runnable{
+
+    @Override
+    public void run() {
+      while(true){
+        synchronized (WaitingThread.class){
+          try {
+            WaitingThread.class.wait();
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
         }
-        Thread.sleep(i * 10);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
       }
+    }
+  }
+
+
+  static class BlockedThread implements Runnable{
+
+    @Override
+    public void run() {
+      while(true){
+        synchronized (BlockedThread.class){
+          sleepInSeconds(100);
+        }
+      }
+    }
+  }
+
+  static void sleepInSeconds(int s){
+    try {
+      Thread.sleep(s);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
   }
 }
